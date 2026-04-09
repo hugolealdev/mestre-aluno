309a4a616a6287a19217fe11a140b841f029bb27
Hugo Leal <hugoleal91@gmail.com>
2026-04-09 00:03:46 -0300

    fix billing module users dependency

--- apps/api/src/modules/billing/billing.module.ts
+++ apps/api/src/modules/billing/billing.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ContentsModule } from '../contents/contents.module.js';
 import { LessonsModule } from '../lessons/lessons.module.js';
+import { UsersModule } from '../users/users.module.js';
 import { BillingController } from './billing.controller.js';
 import { BillingService } from './billing.service.js';
 
 @Module({
-  imports: [ContentsModule, LessonsModule],
+  imports: [ContentsModule, LessonsModule, UsersModule],
   controllers: [BillingController],
   providers: [BillingService]
 })
